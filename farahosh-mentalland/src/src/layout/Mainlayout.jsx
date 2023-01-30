70ba3478e37cfcd80c0ca93364ad1168263649f0
salehe291abbasi <123852379+salehe291abbasi@users.noreply.github.com>
2023-01-30 23:46:31 +0330

    Update Mainlayout.jsx

--- farahosh-mentalland/src/src/layout/Mainlayout.jsx
+++ farahosh-mentalland/src/src/layout/Mainlayout.jsx
@@ -1,15 +1,9 @@
 import { ThemeProvider } from "@mui/material/styles";
-import rtlPlugin from "stylis-plugin-rtl"
-import { CacheProvider } from "@emotion/react";
-import createCache from "@emotion/cache";
-import {prefixer} from "stylis"
-
 import Grid from "@mui/material/Unstable_Grid2";
-import { theme} from "./themes";
 import { useTheme } from "@mui/material/styles";
 
 const MainLayout = ({children})=>{
-    const currentTheme = useTheme();
+    const theme = useTheme();
 
     return(
 
