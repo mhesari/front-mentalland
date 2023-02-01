bb802459992c32d873381ed47883edcecb8bbab5
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-02-01 14:46:25 +0330

    add home section2

---
+++ farahosh-mentalland/src/Components/Home-main/Home-section2/HomeSection2.jsx
@@ -0,0 +1,43 @@
+import { Button, Grid } from "@mui/material";
+import Pic1 from "../../../assests/image/Group 761.png";
+import Pic2 from "../../../assests/image/Group 762.png";
+import Pic3 from "../../../assests/image/Group 763.png";
+import Pic4 from "../../../assests/image/Group 764.png";
+import styles from "./HomeSection2.style.module.css"
+
+
+const Homesection2 = () => {
+    return ( 
+        <div>
+            <div className="flex flex-col justify-items-center align-middle items-center">
+            <h3 className={styles.section2_title}>Who we are</h3>
+            <p className={styles.section2_text}>Lorem ipsum dolor sit amet consectetur. Pretium rutrum nisi mollis sit tortor proin proin sagittis. Id nec suspendisse lacus erat. Vivamus orci bibendum at purus elit. Vel vehicula donec amet a dolor sollicitudin ut. Lectus cursus ipsum mi feugiat nulla enim. Nisl phasellus viverra quisque egestas in nec luctus ornare amet. In pellentesque volutpat urna ultrices vitae. Sed magna vitae placerat eu leo potenti semper id. Sed elementum eget adipiscing nisl in vestibulum. Volutpat.</p>
+            </div>
+            <div className="flex flex-col align-middle  justify-items-start  items-start">
+            <Grid className="flex" direction="row"
+             justifyContent="center"
+  alignItems="center">
+                <div className={styles.section2_pic}>
+                    <img alt="" src={Pic1} />
+                    <p>Created with love</p>
+                </div>
+                <div className={styles.section2_pic}><div className={styles.section2_pic__shadow}></div> <img alt="" src={Pic2} />
+                <p>art academy added</p>
+                </div>
+                <div className={styles.section2_pic}><div className={styles.section2_pic__shadow}></div><img alt="" src={Pic3} /><p>being the first
+mental website in iran</p>
+                </div>
+                <div className={styles.section2_pic}> <div className={styles.section2_pic__shadow}></div><img alt="" src={Pic4} /> <p>Redesigned,improved
+and expanded</p>
+                </div>
+            </Grid>
+            <div className="w-full flex flex-col align-middle items-center">
+            <Button variant="contained" className={styles.section2_button}>Learn more</Button>
+            </div>
+            
+            </div>
+        </div>
+     );
+}
+ 
+export default Homesection2;
--- farahosh-mentalland/src/pages/Routes/Home.jsx
+++ farahosh-mentalland/src/pages/Routes/Home.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import HomeSection1 from "../../Components/Home-main/Home-section1/HomeSection1";
+import Homesection2 from "../../Components/Home-main/Home-section2/HomeSection2";
 
 
 const Home = () => {
     return ( 
         <Fragment>
             <HomeSection1 />
+            <Homesection2 />
         </Fragment>
      );
 }
