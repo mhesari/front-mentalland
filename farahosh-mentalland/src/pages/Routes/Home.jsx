180ea24bd7f316a29e3c5b3931a1826ecbed91de
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-02-01 20:01:33 +0330

    add section5

--- farahosh-mentalland/src/pages/Routes/Home.jsx
+++ farahosh-mentalland/src/pages/Routes/Home.jsx
@@ -3,6 +3,7 @@ import HomeSection1 from "../../Components/Home-main/Home-section1/HomeSection1"
 import Homesection2 from "../../Components/Home-main/Home-section2/HomeSection2";
 import HomeSection3 from "../../Components/Home-main/Home-section3/HomeSection3";
 import Homesection4 from "../../Components/Home-main/Home-Section4/HomeSection4";
+import Homesection5 from "../../Components/Home-main/Home-section5/HomeSection5";
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
             <Homesection2 />
             <HomeSection3 />
             <Homesection4 />
+            <Homesection5 />
         </Fragment>
      );
 }
