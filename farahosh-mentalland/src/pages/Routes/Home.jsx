083f7cec1cc094dd3a06c97c63d8b7c9ab38bd8f
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-02-01 18:41:14 +0330

    add section 4

--- farahosh-mentalland/src/pages/Routes/Home.jsx
+++ farahosh-mentalland/src/pages/Routes/Home.jsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import HomeSection1 from "../../Components/Home-main/Home-section1/HomeSection1";
 import Homesection2 from "../../Components/Home-main/Home-section2/HomeSection2";
 import HomeSection3 from "../../Components/Home-main/Home-section3/HomeSection3";
+import Homesection4 from "../../Components/Home-main/Home-Section4/HomeSection4";
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <HomeSection1 />
             <Homesection2 />
             <HomeSection3 />
+            <Homesection4 />
         </Fragment>
      );
 }
