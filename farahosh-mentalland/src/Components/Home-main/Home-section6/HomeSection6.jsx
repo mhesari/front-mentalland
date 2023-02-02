96f983420675a50550a40abfe3fab12af382a1db
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-02-02 20:32:34 +0330

    add accordion to section6

---
+++ farahosh-mentalland/src/Components/Home-main/Home-section6/HomeSection6.jsx
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
+import pic1 from "../../../assests/image/Group 7.png";
+import pic2 from "../../../assests/image/Group 6.png";
+import pic3 from "../../../assests/image/Group 9.png";
+
+import styles from "./HomeSection6.style.module.css";
+
+const Homesection6 = () => {
+    const [expanded , setExpanded] = React.useState({
+        "case1": true,
+        "case2" : true,
+        "case3" : true,
+        "case4" : true
+
+});
+    const handlechange = Case =>
+    {
+        if(Case === "case1"){
+            if(expanded[Case] === true){setExpanded((prevExpanded) => ( {...prevExpanded, case1 : false}))}
+            if(expanded[Case] === false){setExpanded((prevExpanded) => ( {...prevExpanded, case1 : true}))}
+        }
+        if(Case === "case2"){
+            if(expanded[Case] === true){setExpanded((prevExpanded) => ( {...prevExpanded, case2 : false}))}
+            if(expanded[Case] === false){setExpanded((prevExpanded) => ( {...prevExpanded, case2 : true}))}
+        }
+        if(Case === "case3"){
+            if(expanded[Case] === true){setExpanded((prevExpanded) => ( {...prevExpanded, case3 : false}))}
+            if(expanded[Case] === false){setExpanded((prevExpanded) => ( {...prevExpanded, case3 : true}))}
+        }
+        if(Case === "case4"){
+            if(expanded[Case] === true){setExpanded((prevExpanded) => ( {...prevExpanded, case4 : false}))}
+            if(expanded[Case] === false){setExpanded((prevExpanded) => ( {...prevExpanded, case4 : true}))}
+        }
+
+    }
+    return (
+        <div className='flex flex-col align-middle justify-items-start'>
+            <h4 className={styles.section6_title}>Most Frequently Asked Questions</h4>
+            <div className='flex flex-row align-middle  justify-around items-center'>
+            <div className={styles.section6_list}>
+            <Accordion className={styles.section6_accordion} onChange={()=>{
+                handlechange("case1")
+            }}>
+        <AccordionSummary
+          expandIcon={ expanded.case1 === true ?<AddOutlinedIcon/> : <RemoveOutlinedIcon/>}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={styles.Accordion_title}>Lorem ipsum dolor sit amet consectetur. Est ut vulputate scelerisque nibh?</Typography>
+        </AccordionSummary>
+        <AccordionDetails expandIcon={<Accordion/>}>
+          <Typography>
+          Lorem ipsum dolor sit amet consectetur. Urna non sit non quisque. Mauris purus neque accumsan purus elementum. Proin faucibus in suspendisse malesuada. Egestas nisl arcu sit non mi a. Tortor sed.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion className={styles.section6_accordion} onChange={()=>{
+                handlechange("case2")
+            }}>
+        <AccordionSummary
+          expandIcon={ expanded.case2 === true ?<AddOutlinedIcon/> : <RemoveOutlinedIcon/>}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Lorem ipsum dolor sit amet consectetur. Donec at ut diam sit vitae gravida nisi viverra augue?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion className={styles.section6_accordion} onChange={()=>{
+                handlechange("case3")
+            }}>
+        <AccordionSummary
+          expandIcon={ expanded.case3 === true ?<AddOutlinedIcon/> : <RemoveOutlinedIcon/>}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion className={styles.section6_accordion} onChange={()=>{
+                handlechange("case4")
+            }}>
+        <AccordionSummary
+          expandIcon={ expanded.case4 === true ?<AddOutlinedIcon/> : <RemoveOutlinedIcon/>}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+        </div>
+        <div>
+            <img alt='' src={pic1} />
+            <img alt='' src={pic2} />
+            <img alt='' src={pic3} />
+        </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Homesection6;
