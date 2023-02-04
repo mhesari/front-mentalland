ef7638b9dad3e86c3ab64a0874dcc6ba0589db3a
salehe291abbasi <saleheabbasi313@gmail.com>
2023-02-04 22:29:36 +0330

    change name

--- farahosh-mentalland/src/pages/Routes/Home.jsx
+++ farahosh-mentalland/src/pages/Routes/Home.jsx
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 import HomeSection1 from "../../Components/Home-main/Home-section1/HomeSection1";
 import Homesection2 from "../../Components/Home-main/Home-section2/HomeSection2";
 import HomeSection3 from "../../Components/Home-main/Home-section3/HomeSection3";
-import Homesection4 from "../../Components/Home-main/Home-section4/HomeSection4"
+import Homesection4 from "../../Components/Home-main/Home-Section4/HomeSection4"
 import Homesection5 from "../../Components/Home-main/Home-section5/HomeSection5";
 import Homesection6 from "../../Components/Home-main/Home-section6/HomeSection6";
 
