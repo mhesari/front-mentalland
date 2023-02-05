00b397273ef5bc5d46ab04e2cc140214ecf6bc00
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-02-05 19:56:42 +0330

    fixed a problem

--- farahosh-mentalland/src/pages/Routes/Home.jsx
+++ farahosh-mentalland/src/pages/Routes/Home.jsx
@@ -2,7 +2,8 @@ import { Fragment } from "react";
 import HomeSection1 from "../../Components/Home-main/Home-section1/HomeSection1";
 import Homesection2 from "../../Components/Home-main/Home-section2/HomeSection2";
 import HomeSection3 from "../../Components/Home-main/Home-section3/HomeSection3";
-import Homesection4 from "../../Components/Home-main/Home-Section4/HomeSection4";
+import Homesection4 from "../../Components/Home-main/Home-section4/HomeSection4";
+import Slid2 from "../../Components/Home-main/Home-section5/HomeSection5";
 import Homesection5 from "../../Components/Home-main/Home-section5/HomeSection5";
 import Homesection6 from "../../Components/Home-main/Home-section6/HomeSection6";
 
@@ -16,6 +17,7 @@ const Home = () => {
             <HomeSection3 />
             <Homesection4 />
             <Homesection5 />
+            {/* <Slid2/> */}
             <Homesection6 />
         </Fragment>
      );
