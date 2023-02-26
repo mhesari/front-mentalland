a432f8af6b032d3ecfc522c910ef66965dc76627
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-02-26 12:37:07 +0330

    fix landing page section2 bugs

--- farahosh-mentalland/src/Components/Home-main/Home-section2/HomeSection2.jsx
+++ farahosh-mentalland/src/Components/Home-main/Home-section2/HomeSection2.jsx
@@ -14,7 +14,7 @@ const Homesection2 = () => {
             <p className={styles.section2_text}>Lorem ipsum dolor sit amet consectetur. Pretium rutrum nisi mollis sit tortor proin proin sagittis. Id nec suspendisse lacus erat. Vivamus orci bibendum at purus elit. Vel vehicula donec amet a dolor sollicitudin ut. Lectus cursus ipsum mi feugiat nulla enim. Nisl phasellus viverra quisque egestas in nec luctus ornare amet. In pellentesque volutpat urna ultrices vitae. Sed magna vitae placerat eu leo potenti semper id. Sed elementum eget adipiscing nisl in vestibulum. Volutpat.</p>
             </div>
             <div className="flex flex-col align-middle  justify-items-center   items-center ">
-            <Grid container
+            <Grid container width={1400}
              justifyContent="center"
   alignItems="center">
   <Grid xs={12} sm={6} md={6} lg={3}>
