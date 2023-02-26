63bb09b950075e3374c6be75ecdff9be74f682a0
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-02-26 12:53:17 +0330

    fix contact us page bugs

--- farahosh-mentalland/src/Components/Contact-us/sec-2/sec_2.jsx
+++ farahosh-mentalland/src/Components/Contact-us/sec-2/sec_2.jsx
@@ -5,22 +5,22 @@ import { Box, Grid } from "@mui/material";
 const ContactSection2 = () => {
     return ( 
         <div className={styles.bg_sec2}>
-        <Box>
+        <Box marginBottom={20} marginTop={20}>
             <Grid container   direction={{md:"row" , xs:"column"}}
   justifyContent="space-between"
   alignItems="center">
-                <Grid xs={3} md={3} lg={3} className={styles.contat_items} container direction={"column"} justifyContent={"space-between"}
+                <Grid xs={12} md={12} lg={3} className={styles.contat_items} container direction={"column"} justifyContent={"space-between"}
                 alignItems={"center"}>
                     <img src={pic1} alt="" />
                     <h5>Email Us</h5>
                     <p>mentalland@gmail.com</p>
                 </Grid>
-                <Grid  xs={3} md={3} lg={3}  className={styles.contat_items}  container  direction={"column"} justifyContent={"space-between"}
+                <Grid  xs={12} md={12} lg={3}  className={styles.contat_items}  container  direction={"column"} justifyContent={"space-between"}
                 alignItems={"center"}>
                     <img src={pic1} alt="" />
                     <h5>Email Us</h5>
                     <p>mentalland@gmail.com</p>
-                </Grid><Grid  xs={3} md={3} lg={3}  className={styles.contat_items} container direction={"column"} justifyContent={"center"}
+                </Grid><Grid  xs={12} md={12} lg={3}  className={styles.contat_items} container direction={"column"} justifyContent={"center"}
                 alignItems={"center"}>
                     <img src={pic1} alt="" />
                     <h5>Email Us</h5>
